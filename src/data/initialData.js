1ccc48da7d5eecbd8d85938d37bdeecfaa635bf5
joseph-vertido <jrvertido@gmail.com>
2026-07-06 04:51:49 +0000

    v4.0.37 - Fix version mismatch

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,5 +1,5 @@
 // App Version
-export const APP_VERSION = '4.0.36';
+export const APP_VERSION = '4.0.37';
 
 // Initial player database from baddixx_players_20260211.csv
 export const initialPlayers = [
